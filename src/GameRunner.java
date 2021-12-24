3ff9dcb4f5fd8fbaa40bfa06082c08b9c3a971f2
eyalgolan <eyalgolan96@gmail.com>
2021-12-24 15:13:19 +0200

    Change small factors of the basic implementation

--- src/GameRunner.java
+++ src/GameRunner.java
@@ -2,14 +2,14 @@ public class GameRunner {
 
     public static void main(String[] args) {
         ChatMediator mediator = new ChatMediatorImpl();
-        Player player1 = new PlayerImpl(mediator, "Pankaj");
-        Player player2 = new PlayerImpl(mediator, "Lisa");
-        Player player3 = new PlayerImpl(mediator, "Saurabh");
-        Player player4 = new PlayerImpl(mediator, "David");
+        Player player1 = new PlayerImpl(mediator, "Ofir");
+        Player player2 = new PlayerImpl(mediator, "Eyal");
+        Player player3 = new PlayerImpl(mediator, "Guy");
+        Player player4 = new PlayerImpl(mediator, "Roi");
         mediator.addPlayer(player1);
         mediator.addPlayer(player2);
         mediator.addPlayer(player3);
         mediator.addPlayer(player4);
-        player1.send("Hi All");
+        player1.send("Hey everyone, lets start?");
     }
 }
